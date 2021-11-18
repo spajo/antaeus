442e4565740a14ecd329547804eae00554538e18
feliks <janicki@spaj.eu>
2021-11-18 19:51:02 +0100

    change docs of PaymentProvider to KDoc format add some planning comments in BillingService

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/external/PaymentProvider.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/external/PaymentProvider.kt
@@ -8,21 +8,21 @@
 
 package io.pleo.antaeus.core.external
 
+import io.pleo.antaeus.core.exceptions.CurrencyMismatchException
+import io.pleo.antaeus.core.exceptions.CustomerNotFoundException
+import io.pleo.antaeus.core.exceptions.NetworkException
 import io.pleo.antaeus.models.Invoice
 
 interface PaymentProvider {
-    /*
-        Charge a customer's account the amount from the invoice.
-
-        Returns:
-          `True` when the customer account was successfully charged the given amount.
-          `False` when the customer account balance did not allow the charge.
-
-        Throws:
-          `CustomerNotFoundException`: when no customer has the given id.
-          `CurrencyMismatchException`: when the currency does not match the customer account.
-          `NetworkException`: when a network error happens.
+    /**
+     *  Charge a customer's account the amount from the invoice.
+     *
+     * @throws CustomerNotFoundException when no customer has the given id.
+     * @throws CurrencyMismatchException when the currency does not match the customer account.
+     * @throws NetworkException when a network error happens.
+     * @return `True` when the customer account was successfully charged the given amount or `False`
+     * when the customer account balance did not allow the charge.
+     *
      */
-
     fun charge(invoice: Invoice): Boolean
 }
--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
@@ -5,5 +5,9 @@ import io.pleo.antaeus.core.external.PaymentProvider
 class BillingService(
     private val paymentProvider: PaymentProvider
 ) {
-// TODO - Add code e.g. here
+    // TODO - Add code e.g. here
+    // EX to handle
+    // `CustomerNotFoundException`: when no customer has the given id.
+    // `CurrencyMismatchException`: when the currency does not match the customer account.
+    // `NetworkException`: when a network error happens.
 }
